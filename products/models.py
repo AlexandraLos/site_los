b53d03563a5b2fe1c0c30f0e3e806972ff063dfc
alexandra.los <will13@tut.by>
2020-02-25 02:08:15 +0300

    first commit

---
+++ products/models.py
@@ -0,0 +1,126 @@
+from django.db import models
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Ingredient(models.Model):
+    """Состав продукта"""
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+
+class Brand(models.Model):
+    """Марка продукта"""
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Марка"
+        verbose_name_plural = "Марки"
+
+class Type(models.Model):
+    """Тип продукта"""
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+
+class Product(models.Model):
+    """Продукт"""
+    title = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Картинка", upload_to="products/")
+    country = models.CharField("Страна", max_length=30)
+    brand = models.ManyToManyField(Brand, verbose_name="марка", related_name="product_brand")
+    ingredients = models.ManyToManyField(Ingredient, verbose_name="состав", related_name="product_ingredients")
+    type = models.ManyToManyField(Type, verbose_name="вид")
+    cost = models.PositiveIntegerField("Стоимость", default=0, help_text="стоимость в долларах")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=130, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+class ProductImages(models.Model):
+    """Дополнительные фотографии продукта"""
+    title = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="product_images/")
+    product = models.ForeignKey(Product, verbose_name="Продукт", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фотография продукта"
+        verbose_name_plural = "Фотографии продукта"
+
+class RatingStar(models.Model):
+    """Звезда рейтинга"""
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return f'{self.value}'
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+        ordering = ["-value"]
+
+class Rating(models.Model):
+    """Рейтинг"""
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    movie = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="продукт")
+
+    def __str__(self):
+        return f"{self.star} - {self.product}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+class Review(models.Model):
+    """Отзывы"""
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey('self', verbose_name="родитель", on_delete=models.SET_NULL, blank=True, null=True)
+    movie = models.ForeignKey(Product, verbose_name="фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.product}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
